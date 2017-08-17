a0a1e500c27f42c682ce0b0b672ceca2653df0bc
Sherlin Cruz <sherlincruzP361@gmail.com>
2017-08-16 20:04:48 -0700

    MakeUpClass

---
+++ src/RobotGraffiti.java
@@ -0,0 +1,30 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+public static void main(String[] args) {
+
+	Robot josh = new Robot();
+
+	josh.penDown();
+	josh.setSpeed(10);
+	josh.move(250);
+	josh.turn(90);
+	josh.move(180);	
+	josh.turn(90);
+	josh.move(250);
+	josh.turn(180);
+	josh.move(100);
+	josh.turn(270);
+	josh.move(180);
+	
+	
+	
+}
+
+
+
+
+	
+	
+	
+}
---
+++ src/knockknock.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class knockknock {
+	
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Knock Knock");
+		JOptionPane.showInputDialog("Ice cream");
+		JOptionPane.showInputDialog("Ice cream when i see your face");
+
+}
+}
